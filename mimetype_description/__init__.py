cd83da6555236b748d460b9672c4d798a5398fa2
Christian Gutierrez <chesstrian@gmail.com>
2025-03-04 21:21:48 -0500

    Drop support for py<37

--- mimetype_description/__init__.py
+++ mimetype_description/__init__.py
@@ -1,10 +1,6 @@
 from typing import Union
 
-try:
-    import importlib.resources as pkg_resources
-except ImportError:
-    # Try backported to PY<37 `importlib_resources`.
-    import importlib_resources as pkg_resources
+import importlib.resources as pkg_resources
 import xml.etree.ElementTree
 
 
